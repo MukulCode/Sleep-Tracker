c0723dfff072836df3b787b480db7cc8a2a25cb7
Mukul Kumar <mukul.2121@gmail.com>
2020-11-30 22:46:36 +0530

    Created Dao for the entity to map kotlin functions

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -16,4 +16,32 @@
 
 package com.example.android.trackmysleepquality.database
 
-interface SleepDatabaseDao
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface SleepDatabaseDao{
+    @Insert
+    fun insert(night: SleepNight)
+
+    @Update
+    fun update(night: SleepNight)
+
+
+    //Return thr rows where nightId is equal to the key(we will pass this in kotlin function)
+    // function will return the object of class SleepNight
+    @Query("SELECT * FROM daily_sleep_quality_table WHERE nightId = :key")
+    fun get(key: PrimaryKey):SleepNight
+
+
+    //This will clear the table without knowing about its content
+    @Query("DELETE FROM daily_sleep_quality_table")
+    fun clear()
+
+    //Get all nights, this will return the Live Data wrapped list of all sleepNight Rows
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
+    fun getAllNights():LiveData<List<SleepNight>>
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC Limit 1")
+    fun getTonight():SleepNight?
+}
